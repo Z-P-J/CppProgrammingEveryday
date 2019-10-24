47011f8d6a5b1e3c9450d6056f4465dee6a25653
Z-P-J <37832345+Z-P-J@users.noreply.github.com>
2019-10-24 23:27:14 +0800

    day1

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # CppProgrammingEveryday
  C++编程练习
+
+# 目录
+day1. [Hello World!](https://github.com/Z-P-J/CppProgrammingEveryday/tree/master/src/day1)
--- src/day1/day1.cpp
+++ src/day1/day1.cpp
@@ -1,7 +1,39 @@
 #include "iostream"
+#include "math.h"
 using namespace std;
+
+/*
+编写一个求方程aX2 + bx + c = 0的根的程序，
+要求从控制台输入a, b, c的值并输出结果
+*/
 
 int main()
 {
     cout << "Hello World!" << endl;
+    double a, b, c;
+    cout << "请输入a, b, c的值：";
+    cin >> a >> b >> c;
+    double delta = b * b - 4 * a * c;
+    if (delta > 0)
+    {
+        double sqrt_delta = sqrt(delta);
+        double x1 = (-b + sqrt_delta) / 2 / a;
+        double x2 = (-b - sqrt_delta) / 2 / a;
+        cout << "x1 = " << x1 << "\tx2 = " << x2 << endl;
+    } 
+    else if (delta == 0)
+    {
+        double sqrt_delta = sqrt(delta);
+        double x1 = (-b + sqrt_delta) / 2 / a;
+        cout << "x1 = x2 = " << x1 << endl;
+    } 
+    else 
+    {
+        double sqrt_delta = sqrt(-delta);
+        double positive = -b / 2 / a;
+        double negative = abs(sqrt_delta / 2 / a);
+        cout << "x1 = " << positive << " + " << negative << "i"
+        << "\tx2 = " << positive << " - " << negative << "i" << endl;
+    }
+    system("pause");
 }
